b28a3369a4570729a2d709368ca89567b77384ed
Darcy Linde <lindedarcy@gmail.com>
2019-04-22 16:39:02 -0400

    Done.

--- index.js
+++ index.js
@@ -9,3 +9,22 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+
+function greet(str) {
+  if (str.length == 4) {
+    var hour = str.slice(0,1);
+  } else {
+    var hour = str.slice(0,2);
+  };
+  if (hour < 12) {
+    return "Good Morning";
+  } else if (hour < 17) {
+    return "Good Afternoon";
+  } else {
+    return "Good Evening";
+  };
+}
+
+function displayMessage(str) {
+  document.getElementById("greeting").textContent = str;
+}
